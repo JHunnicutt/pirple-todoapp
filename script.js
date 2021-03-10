5ad64cefadcbe5dd9a1b67ec4f43da452c07ad29
Jordan Hunnicutt <jordanneilhunnicutt@gmail.com>
2021-03-10 08:19:03 -0500

    added login and sign up buttons

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 const mainSection = document.getElementById('main-section');
+const btnDiv = document.createElement('div');
+const btnLogin = document.createElement('button');
+const btnSignup = document.createElement('button');
+
+window.addEventListener('load', () => {
+    btnDiv.classList.add('btn-login-div');
+    btnLogin.innerText = 'Login';
+    btnLogin.classList.add('btn-intro', 'btn-login');
+    btnSignup.innerText = 'Sign Up';
+    btnSignup.classList.add('btn-intro', 'btn-signup');
+    mainSection.appendChild(btnDiv);
+    btnDiv.appendChild(btnLogin);
+    btnDiv.appendChild(btnSignup);
+});
